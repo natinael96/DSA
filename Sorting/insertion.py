4e612c0fde29330c3e9f7a9aa50df3198ae8be09
natinael <natinael.96@gmail.com>
2023-05-20 00:49:40 +0300

    insertion 01

--- Sorting/insertion.py
+++ Sorting/insertion.py
@@ -1,12 +1,14 @@
-A = []
+A = [31, 41, 59, 26, 41, 58]
 
 for i in range(1, len(A)):
     key = A[i]  # store the element at index k as key
 
     j = i - 1   # another var "j" to store the index before key
 
-    while j > -1 and A[j] > key:
+    while j >= 0 and A[j] > key:
         A[j+1] = A[j]
 
-        j += 1
+        j -= 1
     A[j+1] = key
+
+print(A)
