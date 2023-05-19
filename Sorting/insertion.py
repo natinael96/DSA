0dff2b57da4c95b862c056961e74fea498a32a78
natinael <natinael.96@gmail.com>
2023-05-20 00:45:30 +0300

    insertion 01

--- Sorting/insertion.py
+++ Sorting/insertion.py
@@ -1,9 +1,9 @@
 A = []
 
-for k in range(1,len(A)):
-    key = A[k]  # store the element at index k as key
+for i in range(1, len(A)):
+    key = A[i]  # store the element at index k as key
 
-    j = k - 1   # another var "j" to store the index before key
+    j = i - 1   # another var "j" to store the index before key
 
     while j > -1 and A[j] > key:
         A[j+1] = A[j]
