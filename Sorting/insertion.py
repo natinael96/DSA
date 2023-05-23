625a1e3bd0f7a41362f987bf72c4b22156c1dcc6
natinael <natinael.96@gmail.com>
2023-05-23 09:37:21 +0300

    merge

--- Sorting/insertion.py
+++ Sorting/insertion.py
@@ -6,9 +6,10 @@ for i in range(1, len(A)):
     j = i - 1   # another var "j" to store the index before key
 
     while j >= 0 and A[j] > key:
-        A[j+1] = A[j]
 
+        A[j+1] = A[j]
         j -= 1
+
     A[j+1] = key
 
-print(A)
+
