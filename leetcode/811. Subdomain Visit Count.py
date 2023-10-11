fe10caffd36901b04b462c96e3c54e2a89d250df
natinael <natinael.96@gmail.com>
2023-10-11 17:05:29 +0300

    leetcode

---
+++ leetcode/811. Subdomain Visit Count.py
@@ -0,0 +1,18 @@
+class Solution:
+    def subdomainVisits(self, cpdomains: List[str]) -> List[str]:
+        freq = {}
+        for d in cpdomains:
+            count, domains = d.split()
+            count = int(count)
+            
+            subdom = domains.split(".")
+            for i in range(len(subdom)):
+                subdomain = ".".join(subdom[i:])
+                if subdomain not in freq:
+                    freq[subdomain] = count
+                else:
+                    freq[subdomain] += count
+        
+        result = [str(count) + " " + domain for domain,count in freq.items()]
+        return result
+    
