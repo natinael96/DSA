063a709801234d0f079e63912d3fee868fe09d9d
natinael <natinael.96@gmail.com>
2023-10-11 22:47:31 +0300

    leetcode

---
+++ leetcode/75. Sort Colors.py
@@ -0,0 +1,28 @@
+class Solution:
+    def sortColors(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        nums.sort()
+        return nums
+    ###########################
+        low = 0
+        mid = 0
+        high = len(nums) - 1
+        
+        while mid <= high:
+            if nums[mid] == 0:   # need to keep this 0 at low's place
+                nums[low], nums[mid] = nums[mid], nums[low]
+                low += 1
+                mid += 1
+                
+            elif nums[mid] == 1: # 1 is at correct place 
+                mid += 1
+            
+            elif nums[mid] == 2: # need to keep 2 this 2 at high's place
+                nums[mid], nums[high] = nums[high], nums[mid]
+                high -= 1
+        
+        return nums
+
+        
