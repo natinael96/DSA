ef3eaf385c50b6ac1d2a114e78d3d43feb13b3e3
natinael <natinael.96@gmail.com>
2023-10-26 16:43:21 +0300

    progress sheet

--- leetcode/238. Product of Array Except Self.py
+++ leetcode/238. Product of Array Except Self.py
@@ -2,13 +2,13 @@ def productExceptSelf(nums):
     n = len(nums)
     answer = [1] * n
 
-    # Calculate prefix product
+
     prefix_product = 1
     for i in range(n):
         answer[i] *= prefix_product
         prefix_product *= nums[i]
 
-    # Calculate suffix product and multiply with the prefix product
+    
     suffix_product = 1
     for i in range(n - 1, -1, -1):
         answer[i] *= suffix_product
